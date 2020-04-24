33512356428eeb6c5dc8bfde137b50e2e63f00d0
lbirts <birtslauren@gmail.com>
2020-04-24 14:35:57 -0500

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,20 @@
 class Animal
+    attr_accessor :weight, :nickname, :species, :zoo
 
+    @@all = []
+
+    def initialize(species, weight, nickname)
+        @species = species
+        @weight = weight
+        @nickname = nickname
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_species(species)
+        Animal.all.select {|animal| animal.species == species}
+    end
 end
--- run.rb
+++ run.rb
@@ -4,7 +4,39 @@ require 'pry'
 
 
 #Test your code here
+mp_zoo = Zoo.new("Memphis Zoo", "Tennessee")
+bk_zoo = Zoo.new("Brooklyn Zoo", "New York")
+kx_zoo = Zoo.new("Knoxville Zoo", "Tennessee")
+Zoo.all
 
+toni = Animal.new("Tiger", 15, "Toni")
+jerry = Animal.new("Cat", 2.3, "Jerry")
+tom = Animal.new("Rat", 0.6, "Tom")
+snoopy = Animal.new("Dog", 4, "Snoopy")
+garfield = Animal.new("Cat", 5, "Garfield")
+catdog = Animal.new("Cat&Dog", 5, "CatDog")
+Animal.all
+
+tom.zoo = mp_zoo
+toni.zoo = bk_zoo
+jerry.zoo = mp_zoo
+snoopy.zoo = bk_zoo
+garfield.zoo = mp_zoo
+catdog.zoo = bk_zoo
+
+mp_zoo.animals
+bk_zoo.animals
+
+mp_zoo.animal_species
+bk_zoo.animal_species
+
+mp_zoo.find_by_species("Cat")
+
+mp_zoo.animal_nicknames
+bk_zoo.animal_nicknames
+
+Zoo.find_by_location("Tennessee")
+Animal.find_by_species("Cat")
 
 binding.pry
 puts "done"
