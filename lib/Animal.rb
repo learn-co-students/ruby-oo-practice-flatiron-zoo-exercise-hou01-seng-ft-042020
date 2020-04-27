0cb85a8e44025a4d2bc77ec99684495d221f5e50
estherkimyunjung <estherkimyunjung@gmail.com>
2020-04-27 03:32:23 -0500

    Done

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,25 @@
 class Animal
+    attr_reader :species, :nickname
+    attr_accessor :weight, :zoo
+    @@all = []
 
+    def initialize(species, weight, nickname, zoo)
+        @species = species
+        @weight = weight
+        @nickname = nickname
+        @zoo = zoo
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    # def zoo
+    #     self.zoo
+    # end
+
+    def self.find_by_species(find_species)
+        Animal.all.select{|animal| animal.species == find_species}.uniq
+    end
 end
