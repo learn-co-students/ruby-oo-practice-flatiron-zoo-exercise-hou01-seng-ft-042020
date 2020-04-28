e4a94a13ba7bdcea5eb6e1d5aa35eade2f36f2f2
Krysta110 <62581900+Krysta110@users.noreply.github.com>
2020-04-27 23:57:09 -0500

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,29 @@
+# require_relative "Zoo.rb"
+
 class Animal
+
+    attr_reader :nickname, :species
+    attr_accessor :weight, :zoo
+
+    @@all = []
+    def initialize(species, weight, nickname, zoo)
+        @species = species
+        @weight = weight
+        @nickname = nickname
+        @zoo = zoo
+        @@all << self
+    end
+
+
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_species(animal_species)
+        #takes in animal's species
+        #returns an array of all the animals, which are of that species
+        self.all.select {|animal| animal.species == animal_species}
+    end
 
 end
--- run.rb
+++ run.rb
@@ -6,5 +6,26 @@ require 'pry'
 #Test your code here
 
 
+z1 = Zoo.new("Btown Zoo", "Buffalo")
+z2 = Zoo.new("ZooTown", "Buffalo")
+z3 = Zoo.new("Htown Zoo", "Houston")
+
+a1 = Animal.new("giraffee", 225, "Gigi", z3)
+a2 = Animal.new("turtle", 5, "Shelly", z2)
+a3 = Animal.new("bunny", 2, "BunBun", z2)
+a4 = Animal.new("bunny", 3, "Fred", z3)
+a5 = Animal.new("elephant", 435, "Mimi", z3)
+a6 = Animal.new("giraffee", 500, "Spot", z1)
+a7 = Animal.new("elephant", 265, "Adriana", z3)
+
+
+
+
+
+all_animals = Animal.all
+all_elephants = Animal.find_by_species("elephant")
+
+
+
 binding.pry
 puts "done"
